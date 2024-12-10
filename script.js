78097b78e3b7234859526bb50c5920f9f74a7ee2
Christopher Rateng <crateng13@gmail.com>
2024-12-11 01:14:38 +0300

    Initial commit for new landing page

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'primary-bg': '#f8f9fa',
+          'secondary-bg': '#ffffff',
+          'cta': '#f97316',
+        },
+        fontFamily: {
+          sans: ['Arial', 'sans-serif'],
+        },
+      },
+    },
+  };
+  
